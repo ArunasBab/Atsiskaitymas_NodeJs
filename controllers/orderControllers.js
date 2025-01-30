fbd4a3dc5262056244bf58c2ce3cc39f06ea07fb
ArunasBab <arunasbabrovas@gmail.com>
2025-01-30 21:34:45 +0200

    end

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -2,6 +2,32 @@ import mongoose from "mongoose";
 import OrderModel from "../models/OrderModel.js";
 import UserModel from "../models/userModel.js";
 
+export async function getAllOrders(req, res) {
+  try {
+    const orders = await OrderModel.find({}, { __v: 0 });
+
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+export async function getOrderById(req, res) {
+  const { id } = req.params;
+
+  try {
+    const order = await OrderModel.findById(id, { __v: 0 });
+
+    if (!order) {
+      return res.status(404).json({ error: "Order not found" });
+    }
+
+    res.json(order);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
 export async function createNewOrder(req, res) {
   const { userId, product, quantity, status } = req.body;
 
@@ -32,5 +58,44 @@ export async function createNewOrder(req, res) {
     res.json(orderResponse);
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function updateOrderById(req, res) {
+  const { id } = req.params;
+  const { userId, product, quantity, status } = req.body;
+
+  try {
+    const order = await OrderModel.findById(id);
+
+    if (!order) {
+      return res.status(404).json({ error: "Order not found" });
+    }
+
+    order.product = product;
+    order.quantity = quantity;
+    order.status = status;
+
+    await order.save();
+
+    res.json(order);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+export async function deleteOrderById(req, res) {
+  const { id } = req.params;
+
+  try {
+    const deleteOrder = await OrderModel.findByIdAndDelete(id);
+
+    if (!deleteOrder) {
+      return res.status(404).json({ error: "Order not found" });
+    }
+
+    res.json(deleteOrder);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
 }
--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,16 +1,22 @@
 import express from "express";
-import { createNewOrder } from "../controllers/orderControllers.js";
+import {
+  createNewOrder,
+  deleteOrderById,
+  getAllOrders,
+  getOrderById,
+  updateOrderById,
+} from "../controllers/orderControllers.js";
 
 const router = express.Router();
 
-router.get("/orders");
+router.get("/orders", getAllOrders);
 
-router.get("/orders/:id");
+router.get("/orders/:id", getOrderById);
 
 router.post("/orders", createNewOrder);
 
-router.put("/orders/:id");
+router.put("/orders/:id", updateOrderById);
 
-router.delete("/orders/:id");
+router.delete("/orders/:id", deleteOrderById);
 
 export default router;
